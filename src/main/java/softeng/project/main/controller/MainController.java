45c7c064110b52e2eb231a022a3257890f6ea3c5
anick007 <68749413+anick007@users.noreply.github.com>
2021-04-02 00:01:23 -0700

    Menu Design and Controller Class Implementation

---
+++ src/main/java/softeng/project/main/controller/MainController.java
@@ -0,0 +1,15 @@
+package softeng.project.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String homePage(){
+        return "home.html";
+
+
+    }
+}
