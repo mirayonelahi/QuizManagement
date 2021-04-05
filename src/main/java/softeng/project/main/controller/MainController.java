1516db3aeb24b1a4f62ecab4a1076491934c40e0
anick007 <68749413+anick007@users.noreply.github.com>
2021-04-04 22:35:46 -0700

    Updated HomePage Design

--- src/main/java/softeng/project/main/controller/MainController.java
+++ src/main/java/softeng/project/main/controller/MainController.java
@@ -2,14 +2,19 @@ package softeng.project.main.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String homePage(){
+    public String homePage() {
         return "home.html";
-
-
     }
-}
+    @GetMapping("/quiz")
+    public String quiz(){
+            return "quiz.html";
+
+        }
+    }
+
