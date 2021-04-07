492297a10f88f8187b72b89eb7b6b4fcd422100b
anick007 <68749413+anick007@users.noreply.github.com>
2021-04-07 03:38:25 -0700

    Quiz Page Designed

--- src/main/java/softeng/project/main/controller/MainController.java
+++ src/main/java/softeng/project/main/controller/MainController.java
@@ -1,8 +1,11 @@
 package softeng.project.main.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MainController {
@@ -11,10 +14,20 @@ public class MainController {
     public String homePage() {
         return "home.html";
     }
-    @GetMapping("/quiz")
-    public String quiz(){
-            return "quiz.html";
 
-        }
+    @GetMapping("/quiz")
+    public String quiz() {
+        return "quiz.html";
+
     }
+
+    @PostMapping("/questions")
+    public String questions(@RequestParam String username, Model m, RedirectAttributes ra) {
+        if (username.equals("")) {
+            ra.addFlashAttribute("warning", "You must enter your name");
+            return "redirect:/quiz";
+        }
+        return "quiz.html";
+    }
+}
 
