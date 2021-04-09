78f9e51a4b39fe7f1bca4cce5dd8a0855f0b8955
anick007 <68749413+anick007@users.noreply.github.com>
2021-04-08 22:08:59 -0700

    Initial Questions Added to Database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
 insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
 values(
 	1, 
-	'What is a correct syntax to output "Hello World" in Java?',  
-	'echo "Hello World"',
-	'printf("Hello World")',
-	'System.out.println("Hello World")',
+	'What is the single main reason why methods of software requirements analyses fail?',
+	'The models cross over with each other',
+	'Lack of funding',
+	'Requirements change over time',
 	3,
 	-1)
 ;
@@ -12,65 +12,65 @@ values(
 insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
 values(
 	2, 
-	'Java is short for "JavaScript."',  
-	'True',
-	'False',
-	'Partially True',
-	2,
-	-1)
-;
-
-insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
-values(
-	3, 
-	'How do you insert COMMENTS in Java code?',  
-	'# This is a comment',
-	'// This is a comment',
-	'/* This is a comment',
-	2,
-	-1)
-;
-
-insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
-values(
-	4, 
-	'Which data type is used to create a variable that should store text?',  
-	'String',
-	'Char',
-	'Both',
+	'User interviews to see if requirements/User Stories match the user expectations of how the system will perform are examples of:',
+	'Validation',
+	'Verification',
+	'None',
 	1,
 	-1)
 ;
 
 insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
 values(
-	5, 
-	'How do you create a variable with the numeric value 5?',  
-	'num x = 5',
-	'float x = 5',
-	'int x = 5',
-	3,
+	3, 
+	'If a test fails, it could mean:',
+	'The test itself is incorrect',
+	'The program is incorrect',
+	'There is no problem',
+	1,
 	-1)
 ;
 
 insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
 values(
-	6, 
-	'How do you create a variable with the floating number 2.8?',  
-	'num x = 2.8',
-	'float x = 2.8',
-	'int x = 2.8',
+	4, 
+	'Which potentially difficult element will real-time applications add to a testing exercise:',
+	'performance',
+	'time',
+	'reliability',
 	2,
 	-1)
 ;
 
 insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
 values(
+	5, 
+	'The most common definition for unit in the context of unit testing is:',
+	'the smallest testable part of an application',
+	'the part of the application that has errors',
+	'the part that returns method values',
+	1,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	6, 
+	'What are the elements of testing in correct order?',
+	'Act, Arrange, Assert',
+	'Arrange, Assert, Act',
+	'Arrange, Act, Assert',
+	3,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
 	7, 
-	'Which method can be used to find the length of a string?',  
-	'getSize()',
-	'length()',
-	'size()',
+	'Mutation testing is a _______ metric',
+	'black-box',
+	'white-box',
+	'functional',
 	2,
 	-1)
 ;
@@ -78,21 +78,21 @@ values(
 insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
 values(
 	8, 
-	'Which operator is used to add together two values?',  
-	'&&',
-	'.add()',
-	'+',
-	3,
+	'The testing method that Checks devising test cases to show that each program function is operational is called',
+	'black-box testing',
+	'glass-box testing',
+	'grey-box testing',
+	1,
 	-1)
 ;
 
 insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
 values(
 	9, 
-	'The value of a string variable can be surrounded by single quotes.',  
-	'True',
-	'False',
-	'Maybe',
+	'Test driven development has these three steps:',
+	'test, review, retest',
+	'fail, fix, refactor',
+	'test, test, test',
 	2,
 	-1)
 ;
@@ -100,10 +100,10 @@ values(
 insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
 values(
 	10, 
-	'Which operator can be used to compare two values?',  
-	'><',
-	'&|',
-	'==',
+	'Which of the following characteristics represents testable software?',
+	'simplicity',
+	'stability',
+	'all of the above',
 	3,
 	-1)
 ;
